d77d7904cd741738c5e8ac1563dc636d363cf082
Metaverseforpc <57561743+vaibhavard@users.noreply.github.com>
2023-11-14 11:36:43 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request, url_for, redirect
+import subprocess
+import os
 
+data={}
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
-
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/execute", methods=["POST"])
+def execute():
+    global data
+    code = request.data.decode("utf-8")
+    print(code)
+    try:
+        exec(code,globals())  
+    except Exception as e:
+        print(e)
+        return {"Error":str(e)}
+    return data
